b62663303ff90a6a6bc134de212617850700042f
JaksaVucicevic <jaksa.vucicevic@gmail.com>
2012-02-21 17:49:33 +0100

    everything is done, this version works

---
+++ main.py
@@ -0,0 +1,32 @@
+import os, glob, string
+
+def FileList(path):
+    return glob.glob(path+"/*.txt")
+
+def CheckForN(line):
+    if string.find(line,": N") == -1 :
+      return False
+    else :
+      return True  
+
+def FixFile(fname) :
+    F = open(fname)   
+    lines = F.readlines()
+    F.close()
+    F = open(fname,'w')
+    for line in lines :
+        if not CheckForN(line) :
+            F.write(line)
+        else :
+            F.write("Sex: M\n")
+    F.close()  
+
+Flist = FileList('cleandata')
+for Fname in Flist :
+    F = open(Fname)
+    for line in F :
+        if CheckForN(line) :
+             print Fname, line
+             FixFile(Fname)
+ 
+
